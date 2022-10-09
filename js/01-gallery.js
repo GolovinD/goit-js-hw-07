f98ab3f08c27fad4d5451695ac5a863656e4cb75
GolovinD <golovin.dmytro@gmail.com>
2022-10-09 14:16:24 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,70 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery') 
+
+const makeGallery = galleryItems => {
+   
+    return galleryItems.map(item => {
+        const galleryItem = document.createElement('div');
+        galleryItem.classList.add('gallery__item')
+
+        const galleryLink = document.createElement('a');
+        galleryLink.href = (item.original);
+        galleryLink.classList.add = ('.gallery__link');
+        galleryItem.appendChild(galleryLink);
+        
+        const galleryImg = document.createElement('img');
+        galleryImg.classList.add('gallery__image');
+        galleryImg.src = (item.preview);
+        galleryImg.alt = (item.description);
+        galleryImg.setAttribute ('data-source', item.original);
+        galleryLink.appendChild(galleryImg);
+        
+        
+        
+        galleryRef.append(galleryItem);
+        return galleryItem;
+        
+    });
+}
+console.log(basicLightbox);
+makeGallery(galleryItems);
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('gallery__image')) {
+        const imgLarge = event.target.dataset.source;
+        console.log(imgLarge);
+
+        let modalWindow = basicLightbox.create(`
+       <div class="modal">
+        <img
+        src=${imgLarge} 
+        />
+        </div>`);
+
+        modalWindow.show();
+
+//       const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()  
+        
+        
+        
+       
+    }
+
+}
+
+
